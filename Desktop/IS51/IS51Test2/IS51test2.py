c03600c75736da77692cc033f77a2c11b29499df
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-06 21:30:10 -0700

    finish exam

--- Desktop/IS51/IS51Test2/IS51test2.py
+++ Desktop/IS51/IS51Test2/IS51test2.py
@@ -11,3 +11,17 @@ Compute average grade by average = sum of grades / number of grades in the list
 Determine percentages of grades above average
 if grade > average then add into the percentage of grades above average
 """
+
+infile = open("Final.txt", 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0
+for grade in grades:
+    if grade > average:
+        num += 1
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print("Percentage of grade above average: {0:.2f}%".format(100 * num / len(grades)))
