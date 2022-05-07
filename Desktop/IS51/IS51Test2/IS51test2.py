ad6825f116749b5b99a1fc2a43dbcd410da120e3
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-06 21:21:04 -0700

    finish pseudocode

--- Desktop/IS51/IS51Test2/IS51test2.py
+++ Desktop/IS51/IS51Test2/IS51test2.py
@@ -4,3 +4,10 @@ The grades of 24 students are stored in a text file called "final.txt".
 The number of grades, the average grade, and the grades that are above the average must all be calculated. 
 Then print out the output, "Number of grades", "Average grade", "Percentage of grades above average". 
 """
+
+"""
+Get grade from Final.txt and display the number of grades in the text file
+Compute average grade by average = sum of grades / number of grades in the list
+Determine percentages of grades above average
+if grade > average then add into the percentage of grades above average
+"""
