fc18ae48cb59b6ffe1bcc6d7c6f57f4656b94759
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-06 21:12:30 -0700

    finish structured english

---
+++ Desktop/IS51/IS51Test2/IS51test2.py
@@ -0,0 +1,6 @@
+"""
+On a final exam, the program is attempting to show student grades.
+The grades of 24 students are stored in a text file called "final.txt".
+The number of grades, the average grade, and the grades that are above the average must all be calculated. 
+Then print out the output, "Number of grades", "Average grade", "Percentage of grades above average". 
+"""
